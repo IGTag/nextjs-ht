ed192264f27d1aeb66a94d944c881c741a53d66e
Igor Leandro Dantas Medeiros <141191598+IGTag@users.noreply.github.com>
2024-05-26 21:29:25 -0300

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <p>Root Layout Igor Leandro</p>
+      <body className={`${inter.className} flex gap-2`}>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+export const Navbar = () => {
+  return (
+    <nav
+      className="
+        flex 
+        h-screen
+        flex-col
+        gap-4
+       bg-slate-900
+        border-r
+        border-indigo-400/40
+        hover:border-indigo-400/80
+        w-72
+        p-2
+        "
+    >
+      <div>
+        <img
+          src="./images/mtswhite2.png"
+          alt="Logo MuteiTodos"
+          className="max-w-full p-2"
+        />
+      </div>
+      <ul className=" flex-grow my-4 border-t border-indigo-400/20 hover:border--indigo-400/40">
+        <li className=" my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+          Home
+        </li>
+        <li className=" my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+          Games
+        </li>
+        <li className=" my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+          Top 10
+        </li>
+        <li className=" my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+          Walkthroughs
+        </li>
+      </ul>
+      <ul className="my-4 border-t border-indigo-400/20 hover:border--indigo-400/40">
+        <li className=" my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+          User
+        </li>
+      </ul>
+    </nav>
+  );
+};
