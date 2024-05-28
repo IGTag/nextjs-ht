771828a584042f9a1dc761242251db5a627bdeb3
Igor Leandro Dantas Medeiros <141191598+IGTag@users.noreply.github.com>
2024-05-28 19:01:28 -0300

    add class cn.ts

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,42 +5,52 @@ import {
   Walkthroughs,
   UserIcon,
 } from "@/components";
+import { cn } from "@/helpers/cn";
 
-const NavbarList = ({ children, className }) => {
+type NavbarProps = React.ComponentProps<"nav">;
+type NavbarListProps = React.ComponentProps<"ul">;
+type NavbarListItemsProps = React.ComponentProps<"li">;
+
+const NavbarList = ({ children, className, ...props }: NavbarListProps) => {
   return (
     <ul
-      className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}
+      className={cn(
+        "my-4 border-t border-indigo-400/20 hover:border-indigo-400/40",
+        className
+      )}
+      {...props}
     >
       {children}
     </ul>
   );
 };
 
-const NavbarListItem = ({ children, className }) => {
+const NavbarListItem = ({
+  children,
+  className,
+  ...props
+}: NavbarListItemsProps) => {
   return (
     <li
-      className={`my-2 rounded-lg bg-transparent p-2 hover:bg-indigo-400/50 hover:text-slate-100 cursor-pointer flex gap-2 items-center ${className}`}
+      className={cn(
+        "my-2 rounded-lg bg-transparent p-2 hover:bg-indigo-400/50 hover:text-slate-100 cursor-pointer flex gap-2 items-center",
+        className
+      )}
+      {...props}
     >
       {children}
     </li>
   );
 };
 
-export const Navbar = () => {
+export const Navbar = ({ className, ...props }: NavbarProps) => {
   return (
     <nav
-      className="
-        flex 
-        h-screen
-        flex-col
-       bg-slate-900
-        border-r
-        border-indigo-400/20
-        hover:border-indigo-400/40
-        w-72
-        p-2
-        text-slate-300
-        "
+      className={cn(
+        "flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300",
+        className
+      )}
+      {...props}
     >
       <div className="flex items-center justify-center my-4">
         <img
