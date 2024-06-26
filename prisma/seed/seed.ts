9b41fea56e45968cc26b8db9ec68f87333f725f7
Igor Leandro Dantas Medeiros <141191598+IGTag@users.noreply.github.com>
2024-06-26 15:06:17 -0300

    init prisma models for articles

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,27 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import articles from "../../src/app/data/articles.json";
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log("DB Seed");
+  await seedArticles;
+}
+
+async function seedArticles() {
+  for (let article of articles) {
+    const record = await prisma.article.create({
+      data: {
+        title: article.title,
+        excerpt: article.excerpt,
+        content: article.content,
+        image: article.image,
+        publishedAt: new Date(article.publish_date),
+      },
+    });
+
+    console.log("*** Created Article", record.id, record.title);
+  }
 }
 
 main()
@@ -12,6 +30,6 @@ main()
   })
   .catch(async (e) => {
     console.log(e);
-    await prisma.$disconnect;
+    await prisma.$disconnect();
     process.exit(1);
   });
